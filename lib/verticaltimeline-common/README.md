06aafdadb5cabbf102d4bbacf2160c627cd3cfef
Abel Perez Martinez <abelperezok@gmail.com>
2020-07-03 18:07:21 +0100

    Created verticaltimeline-common package

---
+++ lib/verticaltimeline-common/README.md
@@ -0,0 +1,4 @@
+# verticaltimeline_-_common
+
+Common functionalities that can be used by either the web app or the populate database.
+
---
+++ lib/verticaltimeline-common/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='verticaltimeline-common',
+    version='0.0.1',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(where='.')
+)
